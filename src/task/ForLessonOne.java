093234c3198ac53e5e7e6804e51948b82583e7fb
Stanislav <st989@ya.ru>
2020-08-20 14:07:06 +0400

    Initial commit

---
+++ src/task/ForLessonOne.java
@@ -0,0 +1,15 @@
+package task;
+
+/**
+ * Класс с выполненными заданиями,
+ * создан для первого урока "Java Core. Базовый уровень".
+ *
+ * @version   1.0 20.08.2020
+ * @author    Станислав Ленчицкий
+ */
+public class ForLessonOne {
+    public static void main(String[] args) {
+        System.out.println("Проект создан.");
+
+    }
+}
