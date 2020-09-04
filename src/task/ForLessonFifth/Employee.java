6a0f747af1da31d9f4f2766e67c3e7f69f0dc364
Stanislav <st989@ya.ru>
2020-09-04 22:54:26 +0400

    HWLesson5 1

--- src/task/ForLessonFifth/Employee.java
+++ src/task/ForLessonFifth/Employee.java
@@ -9,9 +9,13 @@ public class Employee {
     private int salary;
     private int age;
 
+<<<<<<< HEAD
+    public Employee(String fullName, String position, String email,
+                    String telephone, int salary, int age) {
+=======
     public Employee (String fullName, String position, String email,
                String telephone,int salary,int age) {
-
+>>>>>>> fb1b839... HWLesson5 1
         this.fullName = fullName;
         this.position = position;
         this.email = email;
@@ -21,31 +25,43 @@ public class Employee {
     }
 
     public int getAge() {
+<<<<<<< HEAD
 
+=======
+>>>>>>> fb1b839... HWLesson5 1
         return age;
     }
 
     public int getSalary() {
+<<<<<<< HEAD
 
+=======
+>>>>>>> fb1b839... HWLesson5 1
         return salary;
     }
 
     public String getEmail() {
-
         return email;
     }
 
     public String getFullName() {
+<<<<<<< HEAD
 
+=======
+>>>>>>> fb1b839... HWLesson5 1
         return fullName;
     }
 
     public String getPosition() {
+<<<<<<< HEAD
 
+=======
+>>>>>>> fb1b839... HWLesson5 1
         return position;
     }
 
     public String getTelephone() {
+<<<<<<< HEAD
 
         return telephone;
     }
@@ -66,5 +82,15 @@ public class Employee {
                 "Зарплата -", salary,
                 "Возраст -", age);
 
+=======
+        return telephone;
+    }
+    public void info() {
+
+        System.out.printf("\nФИО - %-20s\nДолжность - %-20s" +
+                        "\nПочта - %-20s\nТелефон - %-20s\nЗарплата - %-20d\n" +
+                        "Возраст - %-20d\n",
+                fullName, position, email,telephone,salary,age);
+>>>>>>> fb1b839... HWLesson5 1
     }
 }
