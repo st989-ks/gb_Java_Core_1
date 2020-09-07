7b16323c6e1cbb1d9620bead63ec087717fb0786
Stanislav <st989@ya.ru>
2020-09-07 22:36:55 +0400

    HWLesson6 1

--- src/task/ForLessonSixth/animals/Cat.java
+++ src/task/ForLessonSixth/animals/Cat.java
@@ -1,4 +1,29 @@
 package task.ForLessonSixth.animals;
 
-public class Cat {
+public class Cat extends Animal {
+
+    public Cat(String name) {
+
+        super(name);
+        countCat++;
+    }
+
+    @Override
+    public void run(int dist) {
+
+        int limit = 200;
+        if (dist > limit) {
+            System.out.printf("Лимит для котов - %dм.\n", limit);
+            return;
+        }
+        System.out.printf("%s пробежал %d м.\n", name, dist);
+
+    }
+
+    @Override
+    public void swim(int dist) {
+
+        System.out.println("Коты не плавают");
+
+    }
 }
--- src/task/ForLessonSixth/animals/Dog.java
+++ src/task/ForLessonSixth/animals/Dog.java
@@ -1,4 +1,34 @@
 package task.ForLessonSixth.animals;
 
-public class Dog {
+public class Dog extends Animal {
+
+    public Dog(String name) {
+
+        super(name);
+        countDog++;
+    }
+
+    @Override
+    public void run(int dist) {
+
+        int limit = 500;
+        if (dist > limit) {
+            System.out.printf("Лимит для бега у собак - %dм.\n", limit);
+            return;
+        }
+        System.out.printf("%s пробежал %d м.\n", name, dist);
+
+    }
+
+    @Override
+    public void swim(int dist) {
+
+        int limit = 10;
+        if (dist > limit) {
+            System.out.printf("Собаки могут проплыть не больше %dм.\n", limit);
+            return;
+        }
+        System.out.printf("%s проплыл %d м.\n", name, dist);
+
+    }
 }
