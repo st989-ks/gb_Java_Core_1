3057ccfb22fc3bd66283b934dee7d6df89542a16
Stanislav <st989@ya.ru>
2020-09-07 22:36:55 +0400

    HWLesson6 1

--- src/task/ForLessonSixth/Main.java
+++ src/task/ForLessonSixth/Main.java
@@ -1,4 +1,36 @@
 package task.ForLessonSixth;
 
-public class Main {
+import task.ForLessonSixth.animals.*;
+
+import static task.ForLessonSixth.animals.Animal.*;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Animal dog = new Dog("Бобик");
+        Animal dog2 = new Dog("Бобик2");
+        Animal dog3 = new Dog("Бобик3");
+        Animal dog4 = new Dog("Бобик4");
+        Animal cat = new Cat("Мурзик");
+        Animal cat2 = new Cat("Мурзик2");
+        Animal[] animals = {new Dog("Кузя"),
+                            new Cat ("Тузик"),
+                            };
+
+        cat.run(220);
+        dog.run(490);
+        cat.swim(200);
+        dog.swim(9);
+        count();
+
+
+    }
+    public static void count() {
+
+        System.out.printf("Всего животных - %d.\n", count);
+        System.out.printf("Всего котов - %d.\n", countCat);
+        System.out.printf("Всего собак - %d.\n", countDog);
+    }
+
+
 }
--- src/task/ForLessonSixth/animals/Animal.java
+++ src/task/ForLessonSixth/animals/Animal.java
@@ -1,4 +1,29 @@
 package task.ForLessonSixth.animals;
 
-public class Animal {
+public abstract class Animal {
+
+    protected String name;
+    public static int count;
+    public static int countCat;
+    public static int countDog;
+
+    public Animal(String name) {
+
+        this.name = name;
+        count++;
+    }
+
+    public void swim(int dist) {
+
+        System.out.printf("%s проплыл %d м.\n", name, dist);
+    }
+
+    public void run(int dist) {
+
+        System.out.printf("%s пробежал %d м.\n", name, dist);
+    }
+
+
+
+
 }
